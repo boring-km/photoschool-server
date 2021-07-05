39bf0de002b4b47310d106826d77d099fd2b20af
boring-km <ts4840644804@gmail.com>
2021-07-06 08:07:26 +0900

    [master] 자신의 학교 정보 가져오는 서비스 추가

--- api/school_controller.js
+++ api/school_controller.js
@@ -1,4 +1,5 @@
 const service = require('../services/school_services');
+const verify = require('../auth/token_verify');
 
 const searchSchool = async (req, res) => {
   try {
@@ -12,6 +13,21 @@ const searchSchool = async (req, res) => {
   }
 };
 
+const getMySchoolInfo = async (req, res) => {
+  try {
+    const verifyResult = await verify(req);
+    if (verifyResult) {
+      const school = await service.getMySchoolInfo(verifyResult);
+      res.json({ result: school });
+    } else {
+      res.status(401).json({ error: 'Token Error!' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Server Error!' });
+  }
+};
+
 module.exports = {
   searchSchool,
+  getMySchoolInfo,
 };
--- services/school_services.js
+++ services/school_services.js
@@ -16,6 +16,25 @@ const searchSchoolName = (schoolName) => new Promise((resolve) => {
   });
 });
 
+const getMySchoolInfo = (email) => new Promise((resolve) => {
+  db((connection) => {
+    const query = `select S.region, S.schoolName, S.address from School S, User U where S.schoolId = U.schoolId and U.email = '${email}';`;
+    logger.debug(query);
+    connection.query(query, (err, results) => {
+      if (err) {
+        logger.error(`getMySchoolInfo: ${err}`);
+        throw err;
+      }
+      if (!results.isEmpty) {
+        resolve(results[0]);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+});
+
 module.exports = {
   searchSchoolName,
+  getMySchoolInfo,
 };
